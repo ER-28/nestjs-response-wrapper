d7969a2410f749bc71ddf675e9f9359af259b2e6
ER28 <l.guerra-rago@pm.me>
2026-06-04 14:59:43 +0200

    wip

---
+++ apps/example/src/app.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Query, HttpException, HttpStatus } from '@nestjs/common';
+import { AppService } from './app.service';
+import { SkipResponseWrapper } from 'nestjs-response-wrapper';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('paginated')
+  getPaginatedData(@Query('page') page: string) {
+    return this.appService.getPaginatedData(page);
+  }
+
+  @Get('error')
+  getError() {
+    throw new HttpException('Custom error message', HttpStatus.BAD_REQUEST);
+  }
+
+  @Get('raw')
+  getRaw() {
+    return 'This is raw response (excluded by route)';
+  }
+
+  @SkipResponseWrapper()
+  @Get('skipped')
+  getSkipped() {
+    return 'This is raw response (skipped by decorator)';
+  }
+}
---
+++ apps/example/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ResponseWrapperModule } from 'nestjs-response-wrapper';
+
+@Module({
+  imports: [
+    ResponseWrapperModule.forRoot({
+      debug: true,
+      version: '1.0.0',
+      enableGlobalInterceptor: true,
+      excludeRoutes: ['/raw'],
+      includeMeta: true
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
